b13ad07170d0b49e6dbe40dd65fcff7c9bbd034a
Lab-Brat <xoovoox@protonmail.com>
2022-06-13 22:54:32 +0300

    move flag functions to lib root crate

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+pub mod flags {
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::io::BufRead;
+
+    pub fn ascii_artify(text: Vec<String>) {
+        let mut aa: [String; 11] = Default::default();
+
+        for letter in text.join(" ").chars() {
+            if letter.is_alphabetic() { 
+                let letvec = read_a_file(letter);
+                for (i, lv) in letvec.iter().enumerate() {
+                    aa[i].push_str(lv)
+                }
+            }
+        }
+        for line in &aa { println!("{}", line) }
+    }
+
+    fn read_a_file(letter: char) -> Vec<String> {
+        let mut filename = String::from("letters/");
+        filename.push(letter);
+
+        let file = File::open(filename).expect("file wasn't found.");
+        let reader = BufReader::new(file);
+
+        let lines: Vec<String> = reader
+            .lines()
+            .map(|line| line.unwrap().parse::<String>().unwrap())
+            .collect();
+
+        return lines
+    }
+
+    pub fn slash_parser(text: Vec<String>) {
+        let mut text = text.join(" ");
+        
+        let mut count = 0;
+        loop {
+            count += 1;
+            if text.contains("\\n") {
+                text = text.replace("\\n", "\n");
+            } else if text.contains("\\t") {
+                text = text.replace("\\t", "\t");
+            } else if text.contains("\\r") {
+                text = text.replace("\\r", "\r");
+            } else { () }
+            if count == text.len() { break; }
+        }
+        
+        println!("{}", text);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
 use clap::{App, Arg};
+use ego::flags;
 
 fn main() {
     let matches = App::new("ego")
@@ -59,58 +57,10 @@ fn main() {
     let rs = matches.is_present("read_slash");
 
     if ascii {
-        ascii_artify(text)
+        flags::ascii_artify(text)
     } else if rs { 
-        slash_parser(text)
+        flags::slash_parser(text)
     } else {
         print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
     }
-}
-
-fn ascii_artify(text: Vec<String>) {
-    let mut aa: [String; 11] = Default::default();
-
-    for letter in text.join(" ").chars() {
-        if letter.is_alphabetic() { 
-            let letvec = read_a_file(letter);
-            for (i, lv) in letvec.iter().enumerate() {
-                aa[i].push_str(lv)
-            }
-        }
-    }
-    for line in &aa { println!("{}", line) }
-}
-
-fn read_a_file(letter: char) -> Vec<String> {
-    let mut filename = String::from("letters/");
-    filename.push(letter);
-
-    let file = File::open(filename).expect("file wasn't found.");
-    let reader = BufReader::new(file);
-
-    let lines: Vec<String> = reader
-        .lines()
-        .map(|line| line.unwrap().parse::<String>().unwrap())
-        .collect();
-
-    return lines
-}
-
-fn slash_parser(text: Vec<String>) {
-    let mut text = text.join(" ");
-    
-    let mut count = 0;
-    loop {
-        count += 1;
-        if text.contains("\\n") {
-            text = text.replace("\\n", "\n");
-        } else if text.contains("\\t") {
-            text = text.replace("\\t", "\t");
-        } else if text.contains("\\r") {
-            text = text.replace("\\r", "\r");
-        } else { () }
-        if count == text.len() { break; }
-    }
-    
-    println!("{}", text);
 }
