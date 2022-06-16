37d7f1ac28f01caa929f5923b980b646c9d742c2
Lab-Brat <xoovoox@protonmail.com>
2022-06-17 00:32:19 +0300

    make test return TestResult type (replace unwrap with ?)

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,23 +1,28 @@
 use std::fs;
 use assert_cmd::Command;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("ego").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("ego")?;
     cmd.arg("-o").assert().success();
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("ego").unwrap();
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("ego")?;
     cmd.arg("hello").assert().success();
+    Ok(())
 }
 
 #[test] 
-fn hello1() {
+fn hello1() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("ego").unwrap();
+    let expected = fs::read_to_string(outfile)?;
+    let mut cmd = Command::cargo_bin("ego")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
 
