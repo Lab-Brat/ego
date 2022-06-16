0f1edb48d29a5fd70cac7bd6adf4201402309f47
Lab-Brat <xoovoox@protonmail.com>
2022-06-16 23:51:51 +0300

    add unit tests

---
+++ tests/cli.rs
@@ -0,0 +1,23 @@
+use std::fs;
+use assert_cmd::Command;
+
+#[test]
+fn dies_no_args() {
+    let mut cmd = Command::cargo_bin("ego").unwrap();
+    cmd.arg("-o").assert().success();
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("ego").unwrap();
+    cmd.arg("hello").assert().success();
+}
+
+#[test] 
+fn hello1() {
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("ego").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
+}
+
