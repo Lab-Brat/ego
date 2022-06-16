50aa62a55b664e6bad5305a653b9b5b90373071c
Lab-Brat <xoovoox@protonmail.com>
2022-06-17 00:41:37 +0300

    add more tests, use helper function

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,6 +3,16 @@ use assert_cmd::Command;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("ego")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
 #[test]
 fn dies_no_args() -> TestResult {
     let mut cmd = Command::cargo_bin("ego")?;
@@ -17,12 +27,27 @@ fn runs() -> TestResult {
     Ok(())
 }
 
-#[test] 
+#[test]
 fn hello1() -> TestResult {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile)?;
-    let mut cmd = Command::cargo_bin("ego")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello there"], "tests/expected/hello1.txt")
 }
 
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
+}
+
+#[test]
+fn hello1_escape_nl() -> TestResult {
+    run(&["Hello\\nthere", "-e"], "tests/expected/hello1.enl.txt")
+}
